e3642f832fff84cf9fd61dcbbca0deb83af3e5b2
optmsshiv <optmsshiv@gmail.com>
2026-09-20 15:17:41 +0530

    class fix

--- models/Manufacturer.php
+++ models/Manufacturer.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../core/Model.php';
 
-class Category extends Model
+class Manufacturer extends Model
 {
-    protected static string $table = 'categories';
+    protected static string $table = 'manufacturers';
 }
